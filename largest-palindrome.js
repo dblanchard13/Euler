35906286077a52e3880faad71a15f59256b01a04
dblanchard13 <davidb.prof@gmail.com>
2015-04-06 17:43:39 -0400

    Optimization for memory

--- largest-palindrome.js
+++ largest-palindrome.js
@@ -14,19 +14,35 @@ var checkPali = function(num){
   return true;
 };
 
+
 var findPali = function(){
   var a = 999;
   var b = 999;
-  var product;
-  var aOrB = false;
-  var results = [];
+  var result = null;
 
   for(var i=999; i>0; i--){
     for(var y=999; y>0; y--){
       if(checkPali(i*y)){
-        results.push(i*y);
+        result = Math.max(i*y, result);
       }
     }
   }
-  return results.sort(function(a, b){return a-b}).pop();
+  return result;
 }
+
+// var findPali = function(){
+//   var a = 999;
+//   var b = 999;
+//   var product;
+//   var aOrB = false;
+//   var results = [];
+
+//   for(var i=999; i>0; i--){
+//     for(var y=999; y>0; y--){
+//       if(checkPali(i*y)){
+//         results.push(i*y);
+//       }
+//     }
+//   }
+//   return results.sort(function(a, b){return a-b}).pop();
+// }
