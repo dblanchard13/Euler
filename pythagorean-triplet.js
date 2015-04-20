3112abaa9bea5db357a6eb05c469d73b2dc0634e
dblanchard13 <davidb.prof@gmail.com>
2015-04-19 17:50:41 -0700

    Pythag Triplet Init

---
+++ pythagorean-triplet.js
@@ -0,0 +1,18 @@
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+// a < b < c
+
+var tripletFinder = function(){
+  var a, b, c;
+
+
+};
+
+
+
+var pythagoreanChecker = function(a, b, c){
+  return a*a + b*b === c*c && a < b && b < c && a + b + c === 1000;
+}
+
+
+
