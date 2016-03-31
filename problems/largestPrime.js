d9b3d05c1aac71e4a8abf97d017e8755224b7a7d
dblanchard13 <dblanchard@gobalto.com>
2016-03-30 17:35:03 -0700

    Complete solution to largest prime factor

--- problems/largestPrime.js
+++ problems/largestPrime.js
@@ -2,18 +2,32 @@
 
 // What is the largest prime factor of the number 600851475143 ?
 
-// 
-var primeFactor = function(){
-  var num = 600851475143;
-  var newNum = Math.sqrt(num);
+var _ = require('lodash');
 
-  for(var i=0; i<=newNum; i++){
+function largestPrimeFact(num){
+  var root = Math.floor(Math.sqrt(num));
+  var largestPrime = undefined;
 
-    for(var y=2; y<i; y++){
-      if(i%y === 0){
-        return;
-      }
+  _.times(root, function(i){
+    i ++;
+    if(isPrime(i) && num % i === 0){
+      largestPrime = i;
     }
-  }
+  });
 
-}
+  return largestPrime;
+};
+
+function isPrime(num){
+  var prime = true;
+  var root = Math.floor(Math.sqrt(num));
+  _.times(root, function(i){
+    i ++;
+    if(num % i === 0 && i !== 1){
+      prime = false;
+    }
+  })
+  return prime;
+};
+
+console.log(largestPrimeFact(600851475143));
