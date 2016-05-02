6fd2a85a0907659eec55ff4b21a635e1e949e9e8
dblanchard13 <dblanchard@gobalto.com>
2016-05-02 11:06:40 -0700

    Implement a js palindrome checker Initial solution for largest product in a grid

--- problems/checkPalindrome.js
+++ problems/checkPalindrome.js
@@ -1,6 +1,22 @@
 // check if a given string is a palindrome
 
 var func = function(str){
-  var rev = str.reverse();
-  return rev === str;
+  var iterations = Math.floor(str.length/2);
+  var end = str.length - 1;
+  var flag = true;
+
+  for(var i=0; i<=iterations; i++){
+    if(str[i] !== str[end-i]){
+      flag = false;
+    }
+  }
+
+  return flag;
 };
+
+// Ruby Implementation
+//
+// def pally(str)
+//   rev = str.reverse
+//   rev == str
+// end
